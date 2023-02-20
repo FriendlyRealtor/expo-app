f8d0ebc299d33dc34c4296fbabfa57155ff23dc8
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-02-19 19:54:44 -0500

    wip getting code to a focusing point

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
-import React, {useEffect} from 'react';
-import {View, LogBox} from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useEffect } from 'react';
+import {
+  View, LogBox
+} from 'react-native';
 import AppNavigation from './src/navigations';
-import {DefaultTheme} from 'react-native-paper';
 import {ConfigProvider} from './src/config';
 import {AuthProvider} from './src/hooks/useAuth';
-import {authManager} from './src/api';
 
 const App = () => {
-  useEffect(() => {
+	useEffect(() => {
     LogBox.ignoreAllLogs(true);
   }, []);
 
   return (
     <ConfigProvider>
-      <AuthProvider authManager={authManager}>
-        <AppNavigation />
-      </AuthProvider>
-    </ConfigProvider>
+			<AuthProvider>
+				<AppNavigation />
+			</AuthProvider>
+		</ConfigProvider>
+
   );
 };
 export default App;
--- src/api/firebase/firebaseAuthManager.js
+++ src/api/firebase/firebaseAuthManager.js
@@ -15,6 +15,8 @@ import {ErrorCode} from '../../api/ErrorCode';
 const defaultProfilePhotoURL =
   'https://www.iosapptemplates.com/wp-content/uploads/2019/06/empty-avatar.jpg';
 
+
+console.log("claling")
 const validateUsernameFieldIfNeeded = (inputFields, appConfig) => {
   return new Promise((resolve, reject) => {
     const usernamePattern = /^[aA-zZ]\w{3,29}$/;
