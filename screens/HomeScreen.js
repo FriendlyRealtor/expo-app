17410479159f30360e9e171d045e413df1b4ef59
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-02-20 18:54:19 -0500

    handle adding home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,66 @@
-import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
-import { signOut } from 'firebase/auth';
+import React, {useCallback, useState} from 'react';
+import {
+  View,
+	Text,
+	Button
+} from 'react-native';
+import { TextInput } from '../components'
 
+//data test
+import axios from 'axios';
+import { signOut } from 'firebase/auth';
 import { auth } from '../config';
 
 export const HomeScreen = () => {
-  const handleLogout = () => {
+  const [id, setId] = useState();
+  const [crmEstimate, setCrmEstimate] = useState(0);
+
+  const getCrmValuation = useCallback(() => {
+    axios({
+      method: 'get',
+      url: `http://localhost:3000/crm?param=${id}`,
+    })
+      .then(response => {
+        if (response.data) {
+          const {value} = response.data;
+          setCrmEstimate(value);
+        }
+      })
+      .catch(error => {
+        console.log('receingg', error);
+      });
+  }, [id]);
+
+	const handleLogout = () => {
     signOut(auth).catch(error => console.log('Error logging out: ', error));
   };
+
   return (
-    <View style={styles.container}>
-      <Button title='Sign Out' onPress={handleLogout} />
+    <View style={{ flex: 1 }}>
+      <Text
+        style={{
+          textAlign: 'center',
+          fontSize: '12px',
+          fontWeight: 'bold',
+        }}
+      >
+        Get CRM Valuation on the go!
+      </Text>
+      <TextInput
+				value={id}
+				onChangeText={(value) => setId(value)}
+				placeholder="Enter Value"
+        onSubmit={value => setId(value)}
+      />
+      <Button title='Click Me' onPress={getCrmValuation} />
+      <Text
+        style={{
+          textAlign: 'center',
+          fontSize: '24px',
+          fontWeight: 'bold',
+        }}
+      >{`$${crmEstimate}`}</Text>
+			<Button title='Sign Out' onPress={handleLogout} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-});
