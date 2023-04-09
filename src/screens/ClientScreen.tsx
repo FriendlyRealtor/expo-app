00737f48aeaf6a72f921f2d317f689b979430a07
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-04-09 15:05:35 -0400

    modal to update form

--- src/screens/ClientScreen.tsx
+++ src/screens/ClientScreen.tsx
@@ -1,10 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout, Text, List, ListItem, Divider, Card } from '@ui-kitten/components';
-import { View } from 'react-native';
+import { View, Modal, Alert, StyleSheet, Pressable } from 'react-native';
 import * as Linking from 'expo-linking';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+export const AddDeal = ({ modalVisible, setModalVisible }) => {
+	const styles = StyleSheet.create({
+    centeredView: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 22,
+    },
+    modalView: {
+      height: 600,
+			width: "100%",
+      backgroundColor: 'white',
+      borderRadius: 20,
+      padding: 35,
+      alignItems: 'center',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2,
+    },
+    buttonOpen: {
+      backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+      backgroundColor: '#2196F3',
+    },
+    textStyle: {
+      color: 'white',
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: 'center',
+    },
+    close: {
+      position: 'absolute',
+      background: 'red',
+      color: 'white',
+      top: 10,
+      right: 10,
+    },
+  });
+
+	return (
+	<Modal  animationType="slide"
+	transparent={true}
+	visible={modalVisible}
+	onRequestClose={() => {
+		Alert.alert('Modal has been closed.');
+		setModalVisible(!modalVisible);
+	}}
+	>
+		<View style={styles.centeredView}>
+			<View style={styles.modalView}>
+				<Pressable onPress={() => setModalVisible(!modalVisible)} style={styles.close}>
+					<Icon style={{ marginRight: 8 }} name="close" size={24} />
+				</Pressable>
+				<Text>Hello</Text>
+			</View>
+		</View>
+	</Modal>
+	);
+}
+
 export const ClientScreen = (props) => {
+	const [modalVisible, setModalVisible] = useState(false);
+
   const renderItemHeader = (headerProps, item) => (
     <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
       <Text category="h6" {...headerProps} status="info">
@@ -90,11 +166,12 @@ export const ClientScreen = (props) => {
   return (
     <Layout style={{ flex: 1 }}>
       <View>
+				<AddDeal modalVisible={modalVisible} setModalVisible={setModalVisible} />
         <Icon
           name="plus-circle"
           size={24}
           onPress={() => {
-            console.log('edit item');
+           setModalVisible(true);
           }}
           style={{ textAlign: 'right', marginRight: 16, padding: 8 }}
         />
