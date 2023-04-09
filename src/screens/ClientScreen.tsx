c0157b84dd55e5dc091afad0ac1c69ff31ff384c
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-04-09 14:55:52 -0400

    working on crud operations

--- src/screens/ClientScreen.tsx
+++ src/screens/ClientScreen.tsx
@@ -1,35 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Layout, Text, List, ListItem, Divider, Card } from '@ui-kitten/components';
 import { View } from 'react-native';
 import * as Linking from 'expo-linking';
-import { Button } from '../components';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const ClientScreen = (props) => {
-  const [errorState, setErrorState] = useState('');
-
-  const makeCall = () => {
-    Linking.openURL('tel:+12409064819');
-  };
-
-  const sendText = () => {
-    Linking.openURL('sms:+12409064819');
-  };
-
-  const handleDeleteItem = async (index) => {
-    try {
-      // await store.deleteCMAItem(userAuth, user, index);
-      // setLocalCmaRows(store.cmaRows);
-    } catch (error) {
-      setErrorState('error deleting item');
-    }
-  };
-
   const renderItemHeader = (headerProps, item) => (
-    <View {...headerProps}>
-      <Text category="h6">
-        {item.index + 1}.) {item.item.address}
+    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+      <Text category="h6" {...headerProps} status="info">
+        {item.item.address}
       </Text>
+      <View style={{ display: 'flex', flexDirection: 'row' }}>
+        <Icon
+          {...headerProps}
+          name="pencil-square-o"
+          size={24}
+          onPress={() => {
+            console.log('edit item');
+          }}
+        />
+        <Icon
+          {...headerProps}
+          name="trash"
+          size={24}
+          color="red"
+          onPress={() => {
+            console.log('delete item');
+          }}
+        />
+      </View>
     </View>
   );
 
@@ -90,121 +89,118 @@ export const ClientScreen = (props) => {
 
   return (
     <Layout style={{ flex: 1 }}>
-      <Button onPress={() => makeCall()}>
-        <Text>Call</Text>
-      </Button>
-      <Button onPress={() => sendText()}>
-        <Text>Text</Text>
-      </Button>
-      <View style={{ flex: 1, marginTop: 22 }}>
-        <Text category="h6" style={{ marginTop: 24, textAlign: 'center' }}>
-          Deals
-        </Text>
-        <View style={{ textAlign: 'center' }}>
-          <List
-            data={[
-              {
-                address: '123 main st',
-                closingDate: '10/19/2023',
-                status: 'active',
-                data: [
-                  {
-                    name: 'Montrell Jubilee',
-                    tel: '2409064819',
-                    type: 'client',
-                  },
-                  {
-                    name: 'Lender 1',
-                    tel: '2409064819',
-                    type: 'lender',
-                  },
-                  {
-                    name: 'Inspector 1',
-                    tel: '2409064819',
-                    type: 'inspector',
-                  },
-                  {
-                    name: 'Title 1',
-                    tel: '2409064819',
-                    type: 'title',
-                  },
-                  {
-                    name: 'Agent 1',
-                    tel: '2409064819',
-                    type: 'agent',
-                  },
-                ],
-              },
-              {
-                address: '123 main st',
-                closingDate: '10/19/2023',
-                status: 'closed',
-                data: [
-                  {
-                    name: 'MOntrell Jubilee 2',
-                    tel: '2409064819',
-                    type: 'client',
-                  },
-                  {
-                    name: 'Lender 2',
-                    tel: '2409064819',
-                    type: 'lender',
-                  },
-                  {
-                    name: 'Inspector 2',
-                    tel: '2409064819',
-                    type: 'inspector',
-                  },
-                  {
-                    name: 'Title 3',
-                    tel: '2409064819',
-                    type: 'title',
-                  },
-                  {
-                    name: 'Agent 4',
-                    tel: '2409064819',
-                    type: 'agent',
-                  },
-                ],
-              },
-              {
-                address: '12345 main st',
-                closingDate: '10/19/2023',
-                status: 'decline',
-                data: [
-                  {
-                    name: 'MOntrell Jubilee 3',
-                    tel: '2409064819',
-                    type: 'client',
-                  },
-                  {
-                    name: 'Lender 3',
-                    tel: '2409064819',
-                    type: 'lender',
-                  },
-                  {
-                    name: 'Inspector 3',
-                    tel: '2409064819',
-                    type: 'inspector',
-                  },
-                  {
-                    name: 'Title 3',
-                    tel: '2409064819',
-                    type: 'title',
-                  },
-                  {
-                    name: 'Agent 3',
-                    tel: '2409064819',
-                    type: 'agent',
-                  },
-                ],
-              },
-            ]}
-            ItemSeparatorComponent={Divider}
-            renderItem={renderItem}
-          />
-        </View>
+      <View>
+        <Icon
+          name="plus-circle"
+          size={24}
+          onPress={() => {
+            console.log('edit item');
+          }}
+          style={{ textAlign: 'right', marginRight: 16, padding: 8 }}
+        />
       </View>
+      <List
+        data={[
+          {
+            address: '123 main st',
+            closingDate: '10/19/2023',
+            status: 'active',
+            data: [
+              {
+                name: 'Montrell Jubilee',
+                tel: '2409064819',
+                type: 'client',
+              },
+              {
+                name: 'Lender 1',
+                tel: '2409064819',
+                type: 'lender',
+              },
+              {
+                name: 'Inspector 1',
+                tel: '2409064819',
+                type: 'inspector',
+              },
+              {
+                name: 'Title 1',
+                tel: '2409064819',
+                type: 'title',
+              },
+              {
+                name: 'Agent 1',
+                tel: '2409064819',
+                type: 'agent',
+              },
+            ],
+          },
+          {
+            address: '123 main st',
+            closingDate: '10/19/2023',
+            status: 'closed',
+            data: [
+              {
+                name: 'MOntrell Jubilee 2',
+                tel: '2409064819',
+                type: 'client',
+              },
+              {
+                name: 'Lender 2',
+                tel: '2409064819',
+                type: 'lender',
+              },
+              {
+                name: 'Inspector 2',
+                tel: '2409064819',
+                type: 'inspector',
+              },
+              {
+                name: 'Title 3',
+                tel: '2409064819',
+                type: 'title',
+              },
+              {
+                name: 'Agent 4',
+                tel: '2409064819',
+                type: 'agent',
+              },
+            ],
+          },
+          {
+            address: '12345 main st',
+            closingDate: '10/19/2023',
+            status: 'decline',
+            data: [
+              {
+                name: 'MOntrell Jubilee 3',
+                tel: '2409064819',
+                type: 'client',
+              },
+              {
+                name: 'Lender 3',
+                tel: '2409064819',
+                type: 'lender',
+              },
+              {
+                name: 'Inspector 3',
+                tel: '2409064819',
+                type: 'inspector',
+              },
+              {
+                name: 'Title 3',
+                tel: '2409064819',
+                type: 'title',
+              },
+              {
+                name: 'Agent 3',
+                tel: '2409064819',
+                type: 'agent',
+              },
+            ],
+          },
+        ]}
+        ItemSeparatorComponent={Divider}
+        renderItem={renderItem}
+      />
     </Layout>
   );
 };
