97141d3c27e6c8c015825399815198093236ffca
Montrell <mjubilee@leaptodigital.com>
2023-05-03 00:25:38 -0400

    adding feed/post screens

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default {
     slug: 'friendlyrealtor',
     privacy: 'unlisted',
     platforms: ['ios'],
-    version: '1.0.1',
+    version: '1.1.1',
     orientation: 'portrait',
     owner: 'jubileeinvestments',
     icon: './assets/logo.png',
