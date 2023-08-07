4c6985fec1b95fe9c594fd3aa0c5a9385f4f274e
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-08-06 22:57:17 -0400

    uncomment code

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default {
     slug: 'friendlyrealtor',
     privacy: 'unlisted',
     platforms: ['ios'],
-    version: '1.0.15',
+    version: '1.0.16',
     orientation: 'portrait',
     owner: 'jubileeinvestments',
     icon: './assets/logo.png',
