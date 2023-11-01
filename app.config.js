efa4df876672a169feb06404236213eca9e33fe7
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-10-31 22:44:30 -0400

    update config

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default {
     slug: 'friendlyrealtor',
     privacy: 'unlisted',
     platforms: ['ios'],
-    version: '1.0.22',
+    version: '1.0.21',
     orientation: 'portrait',
     owner: 'jubileeinvestments',
     icon: './assets/logo.png',
